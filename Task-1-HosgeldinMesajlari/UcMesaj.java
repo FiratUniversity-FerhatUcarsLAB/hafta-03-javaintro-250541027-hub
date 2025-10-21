ade55ec83fa0c2c5d2dbf12ab279c5c51412338d
250541027_e.zehra_duyar <250541027@firat.edu.tr>
2025-10-21 10:48:24 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Javaya hoş geldiniz");
+        System.out.println("Bilgisayar Bilimine HoşGeldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+}
