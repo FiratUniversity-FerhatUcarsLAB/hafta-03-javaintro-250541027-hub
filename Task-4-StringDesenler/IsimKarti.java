b6d388b6a0ddf4525b33c29d6b9afeaac3813021
250541027_e.zehra_duyar <250541027@firat.edu.tr>
2025-10-22 21:56:58 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main (String []args){
+        String ad = "Emine Zehra  " ;
+        String soyad = " Simsek " ;
+        String ogrno = " 250541027 " ;
+        String bolum = " Yazilim Muhendisi " ;
+        
+        System.out.println("=====================") ;
+        System.out.println("  OGRENCI BILGILERI  ") ;
+        System.out.println("=====================") ;
+        System.out.println("Ad      : "  + ad     ) ;
+        System.out.println("Soyad   : "  + soyad  ) ;
+        System.out.println("No      : "  + ogrno  ) ;
+        System.out.println("Bolum   : "  + bolum  ) ;
+        System.out.println("=====================") ;
+        }
+    }
