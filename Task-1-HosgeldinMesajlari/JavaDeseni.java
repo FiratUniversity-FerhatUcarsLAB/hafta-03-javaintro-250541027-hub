1710fcf21c4347db9130bd7b46796c4293fc152a
250541027_e.zehra_duyar <250541027@firat.edu.tr>
2025-10-22 20:32:18 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,13 @@
+public class JavaDeseni {
+    public static void main (String []args ) {
+    System.out.println("   JAV    VAA");
+    System.out.println("  JAAVV  VVAAA");
+    System.out.println("JAAVVVVVVVAAAAAA");
+    System.out.println("JAAAAVVVVVVAAAAA");
+    System.out.println(" JJAAVVVVVVAA ");
+    System.out.println("  JAVVVVAAA");
+    System.out.println("    JAVVA ");
+    System.out.println("     JAV");
+    System.out.println("      J ") ;
+    }
+}
