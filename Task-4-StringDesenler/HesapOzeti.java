2a6f347d22d0a9ee3293e258be5d7a4a2fc7a34b
250541027_e.zehra_duyar <250541027@firat.edu.tr>
2025-10-23 17:32:51 +0300

    Update HesapOzeti.java

--- Task-4-StringDesenler/HesapOzeti.java
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -1,23 +1,33 @@
-public class HesapOzeti{
-    public static void main(String []args ){
-        String[] urunAdlari = { "Süt", "Ekmek", "Peynir" };
-        int[] miktarlar     = { 2, 3, 1 };
-        double[] birimFiyatlar = { 25.50, 7.00, 120.0 };
-        double genelToplam= 0.0;
-        System.out.println("**************** HESAP ÖZETİ ****************");
-        System.out.printf("%-15s %-8s %10s %12s\n", "ÜRÜN ADI", "MİKTAR", "FİYAT (TL)", "TOPLAM (TL)");
-        System.out.println("---------------------------------------------------");
-        for (int i = 0; i < urunAdlari.length; i++) {
-            double satirToplam = miktarlar[i] * birimFiyatlar[i];
-            genelToplam = genelToplam + satirToplam;
-            System.out.printf("%-15s %-8d %10.2f %12.2f\n", 
-                                urunAdlari[i], 
-                                miktarlar[i], 
-                                birimFiyatlar[i], 
-                                satirToplam);
-        System.out.println("---------------------------------------------------");
-        System.out.printf("%-35s %12.2f\n", "GENEL TOPLAM:", genelToplam);
-        System.out.println("***************************************************");
+public class HesapOzeti {
+    public static void main(String[] args) {
+        
+        String urun1 = "Ekmek";
+        String urun2 = "Sut";
+        String urun3 = "Yumurta";
+
+        int adet1 = 3;
+        int adet2 = 2;
+        int adet3 = 15;
+
+        double fiyat1 = 12.0;
+        double fiyat2 = 25.5;
+        double fiyat3 = 3.5;
+
+        
+        double tutar1 = adet1 * fiyat1;
+        double tutar2 = adet2 * fiyat2;
+        double tutar3 = adet3 * fiyat3;
+        double toplam = tutar1 + tutar2 + tutar3;
+
+        
+        System.out.println("========== Hesap Ozeti ==========");
+        System.out.println("Urun       Adet    Fiyat    Tutar");
+        System.out.println("---------------------------------");
+        System.out.println("Ekmek        " + adet1 + "       " + fiyat1 + "        " + tutar1);
+        System.out.println("Sut          " + adet2 + "       " + fiyat2 + "        " + tutar2);
+        System.out.println("Yumurta     " + adet3 + "       " + fiyat3 + "        " + tutar3);
+        System.out.println("---------------------------------");
+        System.out.println("Toplam Tutar:                " + toplam);
+        System.out.println("=================================");
     }
-}
 }
