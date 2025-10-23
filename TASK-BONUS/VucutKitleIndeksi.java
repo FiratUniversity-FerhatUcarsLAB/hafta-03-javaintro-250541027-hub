c9893c7f197751559e0df293af645949223bf8be
250541027_e.zehra_duyar <250541027@firat.edu.tr>
2025-10-23 09:50:40 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,8 @@
+public class BMIHesabı {
+ public static void main (String[]args){
+    double boy = 1.65;
+    double kilo = 57.2;
+    double sonuc = kilo / (boy*boy);
+    System.out.println("BMIHesabı "+ sonuc );
+ }    
+}
