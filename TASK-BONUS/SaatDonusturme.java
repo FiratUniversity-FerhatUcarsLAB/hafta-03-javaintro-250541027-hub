daa1daa3ba7dec6f69f2b41d412d4e7c6add9e9a
250541027_e.zehra_duyar <250541027@firat.edu.tr>
2025-10-23 17:22:25 +0300

    Update SaatDonusturme.java

--- TASK-BONUS/SaatDonusturme.java
+++ TASK-BONUS/SaatDonusturme.java
@@ -8,12 +8,12 @@ public class SaatDonusturme {
         int dakika = toplamSaniye / 60;
         int saniye = toplamSaniye % 60;
 
-        // Dakika ve saniyeyi her zaman 2 basamaklı göstermek için küçük bir numara:
-        // 100 ekleyip substring(1) alırsak baştaki '1' düşer, örnek: 05 olur
+      
         String dakikaStr = ("" + (100 + dakika)).substring(1);
         String saniyeStr = ("" + (100 + saniye)).substring(1);
 
         System.out.println(saat + ":" + dakikaStr + ":" + saniyeStr);
     }
 }
+
 
