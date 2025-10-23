550451232f47f5bb1e9f6cf84f6ff3c74da26b68
250541027_e.zehra_duyar <250541027@firat.edu.tr>
2025-10-23 09:36:02 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class ASCIIArtIsim {
+public static void main(String[]args){
+System.out.println("ZZZZZ  EEEEE  H   H  RRRR     A     ");
+System.out.println("   Z   E      H   H  R   R   A A    ");
+System.out.println("  Z    EEE    HHHHH  RRRR   AAAAA   ");
+System.out.println(" Z     E      H   H  R  R   A   A   ");
+System.out.println("ZZZZZ  EEEEE  H   H  R   R  A   A   ");                               
+      }
+}
